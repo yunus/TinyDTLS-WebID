2293be087d7085777bc6f64b6ab6fdb93602b9a0
yunusdurmus <yunus@yanis.co>
2014-12-17 16:21:16 +0100

    multiple authorization sessions are supported

--- examples/contiki/dtls-coap-webid.c
+++ examples/contiki/dtls-coap-webid.c
@@ -71,6 +71,21 @@ static const unsigned char ecdsa_pub_key_y[] = {
 			0xD0, 0x43, 0xB1, 0xFB, 0x03, 0xE2, 0x2F, 0x4D,
 			0x17, 0xDE, 0x43, 0xF9, 0xF9, 0xAD, 0xEE, 0x70};
 #ifdef DTLS_WEBID
+
+#ifndef WEBID_MAX_CONCURRENT_AUTHORIZATIONS
+#define WEBID_MAX_CONCURRENT_AUTHORIZATIONS 2
+#endif
+typedef struct session_auth{
+	struct session_auth *next;
+	session_t session;
+	char uri[DTLS_WEBID_MAX_URI_LENGTH];
+	size_t uri_size;
+
+} session_auth_t;
+
+LIST(session_table);
+MEMB(session_mem, struct session_auth, WEBID_MAX_CONCURRENT_AUTHORIZATIONS);
+
 /*
  * For URIs shorter the better upper limit is '50' see DTLS_WEBID_MAX_URI_LENGTH.
  * the uri by default starts with https:// so omitted,
@@ -175,14 +190,25 @@ verify_ecdsa_key(struct dtls_context_t *ctx,
   printf("dtls-webid: In verify ecdsa the uri is -%.*s- with length:%d\n",webid_uri_size,webid_uri,webid_uri_size);
 
   /* FIXME: INSTEAD of URI comparison, the certificate of the server should be CHECKED!!!!*/
-  if (strcmp(webid_uri,"example.org/owner_webid/") != 0 ){
+  if (strcmp((const char*)webid_uri,"example.org/owner_webid/") != 0 ){
 	  char query[200];
-	  sprintf(query,"x=%.*s&y=%.*s&uri=%.*s",key_size,(const char *)other_pub_x,key_size,(const char *)other_pub_y,webid_uri_size,(const char *)webid_uri);
+	  session_auth_t *s;
 
+	  s= memb_alloc(&session_mem);
+	  if(NULL == s){
+		  dtls_warn("The session table for authorizations is full. We cannot authorize %.*s\n",webid_uri_size, webid_uri);
+		  return NOT_AUTHORIZED;
+	  }
+	  dtls_session_init(&s->session);
+	  dtls_session_copy(session,&s->session);
+	  s->uri_size = webid_uri_size;
+	  memcpy(s->uri,webid_uri,webid_uri_size);
+	  list_add(session_table,s);
+
+	  sprintf(query,"x=%.*s&y=%.*s&uri=%.*s",key_size,(const char *)other_pub_x,key_size,(const char *)other_pub_y,webid_uri_size,(const char *)webid_uri);
 	  printf("dtls-webid: delegating -%s- \n",query);
 	  process_post_synch(&coaps_delegator,delegation_event, query);
-	  dtls_session_init(&authorization_session);
-	  dtls_session_copy(session,&authorization_session);
+
 	  return WAIT_AUTHORIZATION;
   }
 #endif /* DTLS_WEBID */
@@ -407,16 +433,27 @@ void
 client_chunk_handler(void *response)
 {
   const uint8_t *chunk;
+  session_auth_t *s=NULL;
 
   int len = coap_get_payload(response, &chunk);
-  printf("dtls-webid Chunk: |%.*s\n", len, (char *)chunk);
+  printf("dtls-webid: Waiting sessions are %d, Chunk: |%.*s|\n",list_length(session_table), len, (char *)chunk);
 
-  if(strncmp((const char*) chunk,"verified",8) == 0){
-	  authorized_finish(coap_default_context,&authorization_session,AUTHORIZED);
-  } else {
-	  authorized_finish(coap_default_context,&authorization_session,NOT_AUTHORIZED);
+  for(s = list_head(session_table); s != NULL && strncmp(s->uri,((const char*)&chunk[1]),len-1) != 0; s = s->next) {}
+
+  if(NULL == s){
+	  dtls_warn("The stored session is missing. There is a problem with authorization server \n");
+	  return;
   }
 
+  if(chunk[0] == '1'){
+	  authorized_finish(coap_default_context,&s->session,AUTHORIZED);
+  } else {
+	  authorized_finish(coap_default_context,&s->session,NOT_AUTHORIZED);
+  }
+
+  list_remove(session_table,s);
+  memb_free(&session_mem,s);
+  printf("dtls-webid: Waiting sessions are %d \n",list_length(session_table));
 }
 
 
@@ -429,6 +466,10 @@ PROCESS_THREAD(coaps_delegator, ev, data)
   static coap_packet_t request[1]; /* This way the packet can be treated as pointer as usual. */
   OWNER_NODE(&owner_ipaddr);
   delegation_event = process_alloc_event();
+
+  memb_init(&session_mem);
+  list_init(session_table);
+
   printf("dtls-webid: Delegation process has started \n");
 
 
