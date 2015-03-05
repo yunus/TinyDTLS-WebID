a181478543facf70fdd6982c16048f7f61dd16f6
yunus <y.durmus@tudelft.nl>
2015-03-05 17:53:42 +0100

    watchdog is disabled for cc2538

--- examples/contiki/project-conf.h
+++ examples/contiki/project-conf.h
@@ -4,19 +4,19 @@
 #define __PROJECT_WEBID_DTLS_CONF_H__
 
 
-#ifndef IEEE802154_CONF_PANID
+#undef IEEE802154_CONF_PANID
 #define IEEE802154_CONF_PANID 0xABCD
-#endif
 
-#ifndef RF_CHANNEL
-#define RF_CHANNEL 26
-#endif
+
 
 
 /* Disabling RDC for demo purposes. Core updates often require more memory. */
 /* For projects, optimize memory and enable RDC again. */
 #undef NETSTACK_CONF_RDC
-#define NETSTACK_CONF_RDC			nullrdc_driver
+#define NETSTACK_CONF_RDC  nullrdc_driver
+
+//#undef NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE
+//#define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE 16
 
 #undef NETSTACK_CONF_WITH_IPV6
 #define NETSTACK_CONF_WITH_IPV6 	1
@@ -27,46 +27,22 @@
 #undef UIP_CONF_LOGGING
 #define UIP_CONF_LOGGING 0
 
-#undef UIP_DS6_CONF_PERIOD
-#define UIP_DS6_CONF_PERIOD (CLOCK_SECOND)
-
-#undef UIP_CONF_TCP
-#define UIP_CONF_TCP 0
-
-#undef NETSTACK_CONF_FRAMER
-#define NETSTACK_CONF_FRAMER  framer_802154
-
-#undef CC2520_CONF_AUTOACK
-#define CC2520_CONF_AUTOACK              1
 
 /*Scandium bundles whole package therefore, we need larger uip buffers.
  * If the messages were sent separately, the largest message size should be the target*/
-#undef UIP_CONF_BUFFER_SIZE
-#define UIP_CONF_BUFFER_SIZE 450
-
+//#undef UIP_CONF_BUFFER_SIZE
+//#define UIP_CONF_BUFFER_SIZE 600
+//
 #undef QUEUEBUF_CONF_NUM
-#define QUEUEBUF_CONF_NUM 8
+#define QUEUEBUF_CONF_NUM 10
 
 
-#undef NULLRDC_CONF_802154_AUTOACK
-#define NULLRDC_CONF_802154_AUTOACK      1
-
-#undef NULLRDC_CONF_SEND_802154_ACK
-#define NULLRDC_CONF_SEND_802154_ACK 1
-
-
-#undef RIMESTATS_CONF_ENABLED
-#define RIMESTATS_CONF_ENABLED 0
 
 /* The below is important for wismote, otherwise
  * too may DIS messages (RPL DAG solicitation), normally it is 60
  */
 //#undef RPL_DIS_INTERVAL_CONF
 //#define RPL_DIS_INTERVAL_CONF 6000
-
-/*Openmote*/
-//#define DBG_CONF_USB 1
-#define WATCHDOG_CONF_ENABLE 0
 
 
 //#define UIP_DS6_CONF_NO_STATIC_ADDRESS 1
